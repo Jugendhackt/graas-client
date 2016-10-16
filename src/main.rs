1cb86fd305f0ff13c92e771b97444d8913df8367
sternenseemann <git@lukasepple.de>
2016-10-16 07:03:51 +0200

    Introduce query subcommand
    previous functionality moved to add subcommand

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,8 @@ use hyper::header;
 use std::io::Read;
 use getopts::Options;
 
+const APIURL : &'static str = "http://tmp.pajowu.de/api/data/";
+
 /// DataEntry contains all the data
 /// that is measured by the Geiger-Müller
 /// unit.
@@ -38,66 +40,8 @@ fn to_json(data:DataEntry) -> json::JsonValue {
   return json;
 }
 
-fn main() {
-  let args: Vec<String> = env::args().collect();
-  let progname = args[0].clone();
-
-  let brief = format!("Usage: {} add --time <timestamp> --dosage <dosage> --lat <latitude> --long <longitude>", progname);
-
-  let mut opts = Options::new();
-  opts.optflag("h", "help", "print help menu");
-  opts.optopt("", "time", "unix timestamp of the measurement", "TIMESTAMP");
-  opts.optopt("", "dosage", "dosage in microsievert", "MICROSIEVERT");
-  opts.optopt("", "lat", "latitude of the measurement", "LATITUDE");
-  opts.optopt("", "long", "longitude of the measurement", "LONGITUDE");
-
-  let matches = match opts.parse(args) {
-    Ok(v) => v,
-    Err(e) => panic!(e.to_string()),
-  };
-
-  if matches.opt_present("help") {
-    print!("{}", opts.usage(&brief));
-    return;
-  }
-
-  let dosage = match matches.opt_str("dosage") {
-    Some(s) => match f64::from_str(&s) {
-      Ok(f) => f,
-      Err(e) => panic!("Couldn't parse \"{}\" as float: {}", s, e.to_string())
-    },
-    None => panic!("Option \"dosage\" with argument needed but not provided!")
-  };
-  let time = match matches.opt_str("time") {
-    Some(s) => match i64::from_str(&s) {
-      Ok(i) => i,
-      Err(e) => panic!("Couldn't parse \"{}\" as integer: {}", s, e.to_string()),
-    },
-    None => panic!("Option \"time\" with argument needed but not provided!")
-  };
-  let lat = match matches.opt_str("lat") {
-    Some(s) => match f64::from_str(&s) {
-      Ok(f) => f,
-      Err(e) => panic!("Couldn't \"{}\" as float: {}", s, e.to_string())
-    },
-    None => panic!("Option \"lat\" with argument needed but not provided!")
-  };
-  let long = match matches.opt_str("long") {
-    Some(s) => match f64::from_str(&s) {
-      Ok(f) => f,
-      Err(e) => panic!("Couldn't \"{}\" as float: {}", s, e.to_string())
-    },
-    None => panic!("Option \"long\" with argument needed but not provided!")
-  };
-
-  let data = DataEntry {
-    dosage : dosage,
-    timestamp : time,
-    lat: lat,
-    long: long
-  };
-
-
+/// Adds data to the dataset via the API
+fn insert_data(data:DataEntry) {
   let mut response_body = String::new();
   let mut headers = header::Headers::new();
   let client = client::Client::new();
@@ -106,7 +50,7 @@ fn main() {
 
   headers.set(header::ContentType(mime!(Application/Json)));
 
-  let mut res = client.post("http://tmp.pajowu.de/api/data/")
+  let mut res = client.post(APIURL)
                   .headers(headers)
                   .body(&json)
                   .send().unwrap();
@@ -117,4 +61,96 @@ fn main() {
   let _ = res.read_to_string(&mut response_body);
 
   println!("Body: {}", response_body);
+}
+
+fn query_data() {
+  let client = client::Client::new();
+
+  let mut response_body = String::new();
+  let mut res = client.get(APIURL).send().unwrap();
+
+  let _ = res.read_to_string(&mut response_body);
+
+  let measurements = match json::parse(response_body.as_str()) {
+    Err(e) => panic!(e.to_string()),
+    Ok(j) => j
+  };
+
+  if !measurements.is_array() {
+    panic!("Unexpected data format!");
+  }
+
+
+  for m in measurements.members() {
+    println!("Dosage: {} Time: {} Lat: {} Long: {}", 
+             m["uSv"],
+             m["time"],
+             m["location"]["coordinates"][0],
+             m["location"]["coordinates"][1]);
+  }
+}
+
+fn main() {
+  let args: Vec<String> = env::args().collect();
+  let progname = args[0].clone();
+
+  let brief = format!("Usage: {} add --time <timestamp> --dosage <dosage> --lat <latitude> --long <longitude>
+     | {} query", progname, progname);
+
+  let command = args[1].clone();
+
+  match command.as_str() {
+    "add" => {
+      let mut opts = Options::new();
+      opts.optopt("", "time", "unix timestamp of the measurement", "TIMESTAMP");
+      opts.optopt("", "dosage", "dosage in microsievert", "MICROSIEVERT");
+      opts.optopt("", "lat", "latitude of the measurement", "LATITUDE");
+      opts.optopt("", "long", "longitude of the measurement", "LONGITUDE");
+
+      let matches = match opts.parse(args) {
+        Ok(v) => v,
+        Err(e) => panic!(e.to_string()),
+      };
+
+      let dosage = match matches.opt_str("dosage") {
+        Some(s) => match f64::from_str(&s) {
+          Ok(f) => f,
+          Err(e) => panic!("Couldn't parse \"{}\" as float: {}", s, e.to_string())
+        },
+        None => panic!("Option \"dosage\" with argument needed but not provided!")
+      };
+      let time = match matches.opt_str("time") {
+        Some(s) => match i64::from_str(&s) {
+          Ok(i) => i,
+          Err(e) => panic!("Couldn't parse \"{}\" as integer: {}", s, e.to_string()),
+        },
+        None => panic!("Option \"time\" with argument needed but not provided!")
+      };
+      let lat = match matches.opt_str("lat") {
+        Some(s) => match f64::from_str(&s) {
+          Ok(f) => f,
+          Err(e) => panic!("Couldn't \"{}\" as float: {}", s, e.to_string())
+        },
+        None => panic!("Option \"lat\" with argument needed but not provided!")
+      };
+      let long = match matches.opt_str("long") {
+        Some(s) => match f64::from_str(&s) {
+          Ok(f) => f,
+          Err(e) => panic!("Couldn't \"{}\" as float: {}", s, e.to_string())
+        },
+        None => panic!("Option \"long\" with argument needed but not provided!")
+      };
+
+      let data = DataEntry {
+        dosage : dosage,
+        timestamp : time,
+        lat: lat,
+        long: long
+      };
+
+      insert_data(data);
+    },
+    "query" => query_data(),
+    _ => print!("{}", brief)
+  };
 }
